fe6774f00bce8d69aa924d7f993f11af2f00ede3
Jeffrea <andi.grooms@gmail.com>
2018-08-18 15:10:11 -0500

    Add randrange() function

--- gear.py
+++ gear.py
@@ -3,9 +3,13 @@ from random import randint, random
 def main():
         print("Gear tags are")
         print(randharm())
+        print(randrange())
 
 def randharm():
         return "Harm " + str(randint(1,3))
+
+def randrange():
+        return ["Hand", "Close", "Far"][randint(0,2)]
 
 if __name__ == "__main__":
         # execute only if run as a script
