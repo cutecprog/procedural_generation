97eae6d6582cea1eaef4c47606bdec8c5f6b973f
Jeffrea <andi.grooms@gmail.com>
2018-08-18 15:55:10 -0500

    Add conditions to some tags

--- gear.py
+++ gear.py
@@ -8,20 +8,34 @@ def main():
         shuffle(tags)
         for n in [1.0, 0.85, 0.42, 0.4, 0.4, 0.4]:
                 if random() <= n:
-                        print(tags.pop())
+                        tag = tags.pop()
+                        if tag == "automatic":
+                                # Automatic tag 90% has reload tag
+                                if random() <= 0.9 and "reload" in tags:
+                                        tags.remove("reload")
+                                        print("reload")
+                        elif tag == "quick":
+                                tags.remove("slow")
+                        elif tag == "slow":
+                                tags.remove("quick")
+                        elif tag == "[a material]":
+                                tag = shuffle(materials)[0]
+                        print(tag)
                 else:
                         break
 
 def randharm():
-        return "Harm " + str(randint(1,3))
+        return "harm " + str(randint(1,3))
 
 def randrange():
-        return ["Hand", "Close", "Far"][randint(0,2)]
+        return ["hand", "close", "far"][randint(0,2)]
 
 tags = ["automatic", "area", "balanced", "fire", "healing", "heavy", "holy", \
                 "ignore-armour", "innocuous", "intimate", "loud", "magic", \
                 "many", "[a material]", "messy", "quick", "reload", "slow", \
                 "small", "unreliable", "useful", "valuable", "volatile"]
+
+materials = ["wood", "silver", "copper", "iron", "polycarbon", "bone"]
 
 if __name__ == "__main__":
         # execute only if run as a script
