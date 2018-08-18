cce1a33616420e046750097f25194c14defd262a
Jeffrea <andi.grooms@gmail.com>
2018-08-18 15:07:33 -0500

    Add randharm() function

--- gear.py
+++ gear.py
@@ -1,5 +1,11 @@
+from random import randint, random
+
 def main():
         print("Gear tags are")
+        print(randharm())
+
+def randharm():
+        return "Harm " + str(randint(1,3))
 
 if __name__ == "__main__":
         # execute only if run as a script
