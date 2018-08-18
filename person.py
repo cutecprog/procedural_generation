6f50ded214b896eeafb8364794535c1ed52488ad
Jeffrea <andi.grooms@gmail.com>
2018-08-17 23:44:51 -0500

    Add positive, neutral, negative tuples

--- person.py
+++ person.py
@@ -1,6 +1,11 @@
 def main():
         print("The person is")
 
+positive = (234, 0.37, """""")
+neutral  = (292, 0.18, """""")
+negative = (292, 0.46, """""")
+
+
 if __name__ == "__main__":
         # execute only if run as a script
         main()
