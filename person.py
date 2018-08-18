37d603ef3f6a29acc16bb2ff672b89da9e69f929
Jeffrea <andi.grooms@gmail.com>
2018-08-17 23:54:05 -0500

    Change positive, neutral, negative tuples to dicts

--- person.py
+++ person.py
@@ -2,13 +2,16 @@ from random import randint, random
 
 def main():
         print("The person is")
-        print(randint(0,200))
+        print(randint(0, 200))
         print(random())
-        
+        print(positive["chance"])
+        #for i in range(0, randint(3, 8)):
+        #        if positive[1]
 
-positive = (234, 0.37, """""")
-neutral  = (292, 0.18, """""")
-negative = (292, 0.46, """""")
+
+positive = {"size": 234, "chance": 0.37, "traits": """"""}
+neutral  = {"size": 292, "chance": 0.18, "traits": """"""}
+negative = {"size": 292, "chance": 0.46, "traits": """"""}
 
 
 if __name__ == "__main__":
