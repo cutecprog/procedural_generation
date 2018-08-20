9405d8a1f8c6617e11c9ec3ad17b9de5707e0ea2
Jeffrea <andi.grooms@gmail.com>
2018-08-20 00:22:19 -0500

    Add floraFaunaGen.py file

---
+++ floraFaunaGen.py
@@ -0,0 +1,11 @@
+"""
+Tables by Sebastian Romu
+
+"""
+
+def main():
+        print("Flora and Fauna Generator")
+
+if __name__ == "__main__":
+        # execute only if run as a script
+        main()
