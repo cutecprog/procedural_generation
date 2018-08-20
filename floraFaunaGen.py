e2801ea4f151858508d6862c8d3989af72b3aad2
Jeffrea <andi.grooms@gmail.com>
2018-08-20 15:14:06 -0500

    Change tabs from 8 to 4 spaces

--- floraFaunaGen.py
+++ floraFaunaGen.py
@@ -2,12 +2,13 @@
 Tables by Sebastian Romu
 
 """
+from random import random
 
 def main():
-        print("The flora is")
-        print(vars(flora()))
-        for name in table_names:
-                print("{:>12}: ".format(name))
+    print("The flora is")
+    print(vars(flora()))
+    for name in table_names:
+        print("{:>12}: ".format(name))
         
 
 gravity = 0     # Planetary Gravity Index
@@ -16,18 +17,18 @@ table_names = ["Type", "Habitat", "Grouping", "Size", "Body", "Leaves", \
                 "Reproduction", "Diet", "Sentience", "Edibility"]
 
 class flora(object):
-        def __init__(self):
-                self.type = {}
-                self.habitat = {}
-                self.grouping = {}
-                self.size = {}
-                self.body = {}
-                self.leaves = {}
-                self.reproduction = {}
-                self.diet = {}
-                self.sentience = {}
-                self.edibility = {}
+    def __init__(self):
+        self.type = ""
+        self.habitat = {}
+        self.grouping = {}
+        self.size = {}
+        self.body = {}
+        self.leaves = {}
+        self.reproduction = {}
+        self.diet = {}
+        self.sentience = {}
+        self.edibility = {}
 
 if __name__ == "__main__":
-        # execute only if run as a script
-        main()
+    # execute only if run as a script
+    main()
