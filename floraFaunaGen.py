0d6651b734c5684558cc64d19b11fb9685fee6d8
Jeffrea <andi.grooms@gmail.com>
2018-08-20 01:43:10 -0500

    Add table names list

--- floraFaunaGen.py
+++ floraFaunaGen.py
@@ -5,9 +5,14 @@ Tables by Sebastian Romu
 
 def main():
         print("The flora is")
+        for name in table_names:
+                print("{:>12}: ".format(name))
+        
 
 gravity = 0     # Planetary Gravity Index
 aura = 0        # Planetary Aura Index
+table_names = ["Type", "Habitat", "Grouping", "Size", "Body", "Leaves", \
+                "Reproduction", "Diet", "Sentience", "Edibility"]
 
 if __name__ == "__main__":
         # execute only if run as a script
