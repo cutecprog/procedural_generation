f3ac69489086356d2da1a5b43df79b2c4b6a75a6
Jeffrea <andi.grooms@gmail.com>
2018-08-20 01:22:43 -0500

    Add gravity and aura variables

--- floraFaunaGen.py
+++ floraFaunaGen.py
@@ -4,7 +4,10 @@ Tables by Sebastian Romu
 """
 
 def main():
-        print("Flora and Fauna Generator")
+        print("The flora is")
+
+gravity = 0     # Planetary Gravity Index
+aura = 0        # Planetary Aura Index
 
 if __name__ == "__main__":
         # execute only if run as a script
