cba6a641dc4994c838de86a2c85f8696fb2445e6
Jeffrea <andi.grooms@gmail.com>
2018-08-21 07:28:22 -0500

    Fix bug in roll_twice() function

--- floraFaunaGen.py
+++ floraFaunaGen.py
@@ -9,8 +9,6 @@ def main():
     print("The flora is")
     print("{:>12}: ".format("Type") + flora.type)
     print(vars(flora))
-    print(select(random(), \
-            {"Woody": 0.30, "Herbaceous": 0.85, "Algae": 0.90, "Fungus": 1}))
 
 gravity = 0     # Planetary Gravity Index
 aura = 0        # Planetary Aura Index
@@ -22,11 +20,13 @@ def select(roll, weights):
         # sort table able by weights
         if roll <= weights[tag]:
             return tag
+    print("Error: No value less than or equal to " + roll)
 
 def roll_twice(tag, additional_tag):
     if additional_tag != tag:
         # both rolled subhabitats aren't the same
         return [tag, additional_tag]
+    return tag
 
 type_table = {"Woody": 0.30, "Herbaceous": 0.85, "Algae": 0.90, "Fungus": 1}
 
@@ -46,20 +46,32 @@ subhabitat_table = {"Aquatic": \
         "Marsh / Swamp / Bog": 0.50, "Forest / Jungle": 0.80, \
         "Hills / Scrub": 0.90, "Mountains": 0.95, "Tundra": 1} }
 
+rarity_table = {"Very Common": 0.15, "Common": 0.50, "Uncommon": 0.80, \
+        "Rare": 0.94, "Very Rare": 0.99, "Unique": 1}
+
 class Flora(object):
     def __init__(self):
+        # Table 1
         self.type = select(random(), type_table)
+        # Table 2
         self.habitat = {}
         self.habitat["primary"] = select(random() - 5 * gravity, habitat_table[self.type])
         if self.habitat["primary"] == "Aquatic":
+            # Table 2a
             self.habitat["sub"] = select(random(), subhabitat_table["Aquatic"])
         else:
+            # Table 2b
             self.habitat["sub"] = select(random(), subhabitat_table["Other"])
             if random() <= 0.10:
                 # 10% chance of two subhabitats
                 self.habitat["sub"] = roll_twice(self.habitat["sub"], \
                         select(random(), subhabitat_table["Other"]))
-
+        #table 2c
+        if type(self.habitat["sub"]) is list:
+            pass
+        else:
+            self.habitat["sub"] = \
+                    {self.habitat["sub"]: select(random(), rarity_table)}
         self.grouping = {}
         self.size = {}
         self.body = {}
