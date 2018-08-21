6dfb5aa3e3186c19820d96157e19253c48a42c6d
Jeffrea <andi.grooms@gmail.com>
2018-08-21 08:31:41 -0500

    Add size table

--- floraFaunaGen.py
+++ floraFaunaGen.py
@@ -10,6 +10,8 @@ def main():
     print("The flora is")
     print("{:>12}: ".format("Type") + flora.type)
     print("{:>12}: ".format("Habitat") + flora.habitat["primary"])
+    print("{:>12}: ".format("Grouping") + flora.grouping)
+    print("{:>12}: ".format("Size") + flora.size)
     pprint(vars(flora))
 
 gravity = 0     # Planetary Gravity Index
@@ -29,35 +31,44 @@ def roll_twice(tag, additional_tag):
         # both rolled subhabitats aren't the same
         return [tag, additional_tag]
     return tag
-
+# Table 1
 type_table = {"Woody": 0.30, "Herbaceous": 0.85, "Algae": 0.90, "Fungus": 1}
-
-habitat_table = {"Woody": \
-        {"Aquatic": 0.05, "Sub-Terrestrial": 0.10, "Terrestrial": 0.99, "Avian": 1}, \
-        "Herbaceous": \
-        {"Aquatic": 0.35, "Sub-Terrestrial": 0.40, "Terrestrial": 0.99, "Avian": 1}, \
-        "Algae": \
-        {"Aquatic": 0.80, "Sub-Terrestrial": 0.90, "Terrestrial": 0.98, "Avian": 1}, \
-        "Fungus": \
+# Table 2
+habitat_table = {"Woody":
+        {"Aquatic": 0.05, "Sub-Terrestrial": 0.10, "Terrestrial": 0.99, "Avian": 1},
+        "Herbaceous":
+        {"Aquatic": 0.35, "Sub-Terrestrial": 0.40, "Terrestrial": 0.99, "Avian": 1},
+        "Algae":
+        {"Aquatic": 0.80, "Sub-Terrestrial": 0.90, "Terrestrial": 0.98, "Avian": 1},
+        "Fungus":
         {"Aquatic": 0.10, "Sub-Terrestrial": 0.40, "Terrestrial": 0.99, "Avian": 1} }
-
-subhabitat_table = {"Aquatic": \
-        {"Salt-Water": 0.30, "Fresh Water": 0.90, "Brackish": 1}, \
-        "Other": \
-        {"Desert / Waste": 0.10, "Plains / Savannah": 0.40, \
-        "Marsh / Swamp / Bog": 0.50, "Forest / Jungle": 0.80, \
+# Table 2a, 2b
+subhabitat_table = {"Aquatic":
+        {"Salt-Water": 0.30, "Fresh Water": 0.90, "Brackish": 1},
+        "Other":
+        {"Desert / Waste": 0.10, "Plains / Savannah": 0.40,
+        "Marsh / Swamp / Bog": 0.50, "Forest / Jungle": 0.80,
         "Hills / Scrub": 0.90, "Mountains": 0.95, "Tundra": 1} }
-
-rarity_table = {"Very Common": 0.15, "Common": 0.50, "Uncommon": 0.80, \
+# Table 2c
+rarity_table = {"Very Common": 0.15, "Common": 0.50, "Uncommon": 0.80,
         "Rare": 0.94, "Very Rare": 0.99, "Unique": 1}
-
-grouping_table = {"Woody": \
+# Table 3
+grouping_table = {"Woody":
         {"Solitary": 0.20, "Small Patch": 0.40, "Medium Patch": 0.80, "Large Patch": 1},
-        "Herbaceous": \
+        "Herbaceous":
         {"Solitary": 0.30, "Small Patch": 0.50, "Medium Patch": 0.90, "Large Patch": 1},
-        "Algae": \
+        "Algae":
         {"Solitary": 0.25, "Small Patch": 0.70, "Medium Patch": 0.95, "Large Patch": 1}}
 grouping_table["Fungus"] = grouping_table["Algae"]
+# Table 4
+size_table = {"Woody":
+        {"Huge": 0.15, "Large": 0.45, "Average": 0.80, "Small": 0.98, "Tiny": 1},
+        "Herbaceous":
+        {"Huge": 0.10, "Large": 0.20, "Average": 0.70, "Small": 0.95, "Tiny": 1},
+        "Algae":
+        {"Huge": 0.02, "Large": 0.15, "Average": 0.65, "Small": 0.85, "Tiny": 1},
+        "Fungus":
+        {"Huge": 0.01, "Large": 0.10, "Average": 0.30, "Small": 0.60, "Tiny": 1} }
 
 class Flora(object):
     def __init__(self):
@@ -86,7 +97,8 @@ class Flora(object):
                     {self.habitat["sub"]: select(random(), rarity_table)}
         # Table 3
         self.grouping = select(random(), grouping_table[self.type])
-        self.size = {}
+        # Table 4
+        self.size = select(random(), size_table[self.type])
         self.body = {}
         self.leaves = {}
         self.reproduction = {}
